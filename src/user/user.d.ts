351261a8d599dc678d7dd6b27619b3bbc88e3518
wonju-dev <wonjulee.dev@gmail.com>
2022-02-25 16:34:47 +0900

    feat, refactor#29 : controller, service, repository 추상 클래스 만들기
    controller, service, repository 추상 클래스 만들기
    - 자바 Spring 처럼 완벽한 DI + Singleton을 목적으로 시도했는데, 상당이 어려웠다 (그리고 맞게 한건지도 잘 모르겠다)
        - 별도의 Container가 없어서 힘들다
    - DI를 어디까지 해야할지 아직은 감이 잘 안 잡힌다
    
    prettier의 한 줄 기준을 80 -> 140으로 변경했다
    - 그냥 내 맘
    
    더미 데이터 삭제
    - 이제는 DB랑 소통하자
    
    Repository에서 connection의 영향력을 줄이자
    - typeorm이 익숙하지 않다보니, connection이 얼마나 사용될지 몰라서, app 시작점에서 connection을 통째로 넣어줬다
    - 별로 좋지는 않은 것 같다. DL (dependency lookup)을 적용할 수 있을까?

--- src/user/user.d.ts
+++ src/user/user.d.ts
@@ -1,5 +1,42 @@
-import { Connection } from 'typeorm';
-import UserRepository from './user.repository';
+import { Connection, EntitySchema } from 'typeorm';
+import express, { Request, Response, Router } from 'express';
+
+export abstract class AbstractUserRepository {
+	private static instance: AbstractUserRepository;
+	private static connection: Connection;
+	private static entity: EntitySchema;
+
+	public static getInstance(connection: Connection, entity: EntitySchema): AbstractUserRepository;
+	private constructor(connection: Connection, entity: EntitySchema);
+
+	findUserById(id: number): Promise<User>;
+	findUserByNickname(nickname: string): Promise<User[]>;
+}
+
+export abstract class AbstractUserService {
+	private static instance: AbstractUserService;
+	private static userRepository: AbstractUserRepository;
+
+	public static getInstance(userRepository: AbstractUserRepository): AbstractUserService;
+	private constructor(userRepository: AbstractUserRepository);
+
+	findUserById(id: number): Promise<User>;
+	findUserByNickname(nickname: string): Promise<User[]>;
+}
+
+export abstract class AbstractUserController {
+	private static instance: AbstractUserController;
+	private static userService: AbstractUserService;
+	private static readonly router: Router;
+	private static readonly PATH: string;
+
+	public static getInstance(userService: AbstractUserService, app: express.Application): AbstractUserController;
+	private constructor(userService: AbstractUserService, app: express.Application);
+	initRouter(app: express.Application): void;
+
+	getUserById(req: Request, res: Response): Promise<void>;
+	getUserByNickname(req: Request, res: Response): Promise<void>;
+}
 
 export type User = {
 	id: number;
@@ -12,14 +49,3 @@ export type User = {
 	description: string;
 	grade: number;
 };
-
-export abstract class AbstractUserRepository {
-	private static instance: AbstractUserRepository;
-	private static connection: Connection;
-
-	public static getInstance(connection: Connection): AbstractUserRepository;
-	private constructor(connection: Connection);
-
-	findUserById(id: number): Promise<User>;
-	findUserByNickname(nickname: string): Promise<User[]>;
-}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,30 +1,27 @@
-import UserRepository from './user.repository';
-import { User } from './user';
+import { AbstractUserRepository, AbstractUserService, User } from './user';
 
-export default class UserService {
-	private static instance: UserService;
-	private static userRepository: UserRepository;
+export default class UserService implements AbstractUserService {
+	private static instance: AbstractUserService;
+	private static userRepository: AbstractUserRepository;
 
-	public static getInstance(userRepository: UserRepository): UserService {
+	public static getInstance(userRepository: AbstractUserRepository): AbstractUserService {
 		if (!UserService.instance) {
 			UserService.instance = new UserService(userRepository);
 		}
 		return UserService.instance;
 	}
 
-	private constructor(userRepository: UserRepository) {
+	private constructor(userRepository: AbstractUserRepository) {
 		UserService.userRepository = userRepository;
 	}
 
-	async findUserById(id: number) {
+	async findUserById(id: number): Promise<User> {
 		const findUser = await UserService.userRepository.findUserById(id);
 		return findUser;
 	}
 
-	async findUserByNickname(nickname: string) {
-		const findUserList = await UserService.userRepository.findUserByNickname(
-			nickname
-		);
+	async findUserByNickname(nickname: string): Promise<User[]> {
+		const findUserList = await UserService.userRepository.findUserByNickname(nickname);
 		return findUserList;
 	}
 }
