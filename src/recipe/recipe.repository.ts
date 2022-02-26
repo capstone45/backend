634c01cc128b9d7add435e25f007b85c6c4a7965
wonju-dev <wonjulee.dev@gmail.com>
2022-02-26 16:45:41 +0900

    feat#4 : '최근 날짜 정렬' 검색, '오늘 생성된 레시피 중, 좋아요 많은 순 정렬' 검색 구현
    1. '최근 날짜 정렬' 검색
    - TypeORM의 쿼리 빌더 사용
    
    2. '오늘 생성된 레시피 중, 좋아요 많은 순 정렬' 검색
    - TypeORM의 쿼리 빌더 사용

--- src/recipe/recipe.repository.ts
+++ src/recipe/recipe.repository.ts
@@ -1,10 +1,12 @@
 import { Connection, EntitySchema } from 'typeorm';
+import { getFormattedDate } from '../helper/helper';
 import { Recipe, AbstractRecipeRepository } from './recipe';
 
 export default class RecipeRepository implements AbstractRecipeRepository {
 	private static instance: AbstractRecipeRepository;
 	private static connection: Connection;
 	private static entity: EntitySchema;
+	private LIMIT: 6;
 
 	public static getInstance(connection: Connection, entity: EntitySchema): AbstractRecipeRepository {
 		if (!RecipeRepository.instance) {
@@ -19,7 +21,32 @@ export default class RecipeRepository implements AbstractRecipeRepository {
 	}
 
 	async findRecipeByTitle(title: string): Promise<Recipe[]> {
-		const findResultList = await RecipeRepository.connection.getRepository(RecipeRepository.entity).find({ where: { title: title } });
-		return findResultList;
+		const findRecipeList = await RecipeRepository.connection.getRepository(RecipeRepository.entity).find({ where: { title: title } });
+		return findRecipeList;
+	}
+
+	async findTodaysMostLikedRecipe(): Promise<Recipe[]> {
+		const findRecipeList = await RecipeRepository.connection
+			.getRepository(RecipeRepository.entity)
+			.createQueryBuilder('recipe')
+			.select()
+			.leftJoin('recipe.recipeHasTags', 'rht')
+			.where('recipe.createDate = :today', { today: getFormattedDate(new Date()) })
+			.groupBy('recipe.id')
+			.orderBy('COUNT(rht.id)', 'DESC')
+			.limit(this.LIMIT)
+			.getMany();
+		return findRecipeList;
+	}
+
+	async findLatestCreatedRecipe(): Promise<Recipe[]> {
+		const findRecipeList = await RecipeRepository.connection
+			.getRepository(RecipeRepository.entity)
+			.createQueryBuilder('recipe')
+			.select()
+			.orderBy('recipe.createDate', 'DESC')
+			.limit(this.LIMIT)
+			.getMany();
+		return findRecipeList;
 	}
 }
