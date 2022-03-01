d799dbf81b28963d9714334eb8dfbb0ddec81c2a
wonju-dev <wonjulee.dev@gmail.com>
2022-03-01 13:34:42 +0900

    feat: Entity ㅂ버그 수정, 개선
    OneToMany 관계의 Entity 컬럼의 초기값을 삭제했습니다
    - typeORM은 초기값이 필요없는 듯합니다
    
    일부 테이블을 식별관계로 만들었습니다
    - 식별관계로 만들어서 중복값이 들어가지 못하도록 했습니다

--- src/bookmark/bookmark.entity.ts
+++ src/bookmark/bookmark.entity.ts
@@ -1,13 +1,9 @@
-import { Entity, PrimaryColumn, Generated, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, ManyToOne, JoinColumn } from 'typeorm';
 import User from '../user/user.entity';
 import Recipe from '../recipe/recipe.entity';
 
 @Entity({ name: 'BOOKMARK' })
 export default class Bookmark {
-	@PrimaryColumn({ name: 'BOOKMARK_ID', type: 'bigint', unsigned: true })
-	@Generated('increment')
-	id: number;
-
 	@ManyToOne(() => Recipe, (recipe) => recipe.id, { lazy: true, nullable: false, primary: true })
 	@JoinColumn({ name: 'RECIPE_ID' })
 	recipe: Recipe;
--- src/subscribe/subscribe.entity.ts
+++ src/subscribe/subscribe.entity.ts
@@ -1,17 +1,13 @@
-import { Entity, PrimaryColumn, Generated, ManyToOne, JoinColumn } from 'typeorm';
+import { Entity, ManyToOne, JoinColumn } from 'typeorm';
 import User from '../user/user.entity';
 
 @Entity({ name: 'SUBSCRIBE' })
 export default class Subscribe {
-	@PrimaryColumn({ name: 'SUBSCRIBE,ID', type: 'bigint', unsigned: true })
-	@Generated('increment')
-	id: number;
-
-	@ManyToOne(() => User, (user) => user.id, { lazy: true, nullable: false, primary: true })
+	@ManyToOne(() => User, (user) => user.subscribers, { lazy: true, nullable: false, primary: true })
 	@JoinColumn({ name: 'SUBSCRIBER_ID' })
 	subscriber: User;
 
-	@ManyToOne(() => User, (user) => user.id, { lazy: true, nullable: false, primary: true })
+	@ManyToOne(() => User, (user) => user.publishers, { lazy: true, nullable: false, primary: true })
 	@JoinColumn({ name: 'PUBLISHER_ID' })
 	publisher: User;
 }
