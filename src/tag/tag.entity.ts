1a59446a210c4a53dfc8feecaec3397655fd12d2
wonju-dev <wonjulee.dev@gmail.com>
2022-03-06 18:41:27 +0900

    refactor : tag 개선
    미사용 필드 제거
    DB저장을, 객체 생성 + DB 저장으로 나눴음

--- src/tag/tag.entity.ts
+++ src/tag/tag.entity.ts
@@ -1,6 +1,4 @@
-import { Entity, PrimaryColumn, Generated, Column, OneToMany } from 'typeorm';
-
-import RecipeTag from '../recipe-tag/recipe-tag.entity';
+import { Entity, PrimaryColumn, Generated, Column } from 'typeorm';
 
 @Entity({ name: 'TAG' })
 export default class Tag {
@@ -8,9 +6,12 @@ export default class Tag {
 	@Generated('increment')
 	id: number;
 
-	@OneToMany(() => RecipeTag, (recipeTag) => recipeTag.tag, { lazy: true })
-	recipes: RecipeTag[];
-
 	@Column({ name: 'NAME', length: 24, nullable: false })
 	name: string;
+
+	static create(tagName: string): Tag {
+		const tag = new Tag();
+		tag.name = tagName;
+		return tag;
+	}
 }
