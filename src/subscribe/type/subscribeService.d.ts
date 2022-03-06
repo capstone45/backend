4fd33265bd95665460919289c5b7b69924281a75
wonju-dev <wonjulee.dev@gmail.com>
2022-03-07 01:41:33 +0900

    feat : subscribe Repo, service 타입 구성

--- src/subscribe/type/subscribeService.d.ts
+++ src/subscribe/type/subscribeService.d.ts
@@ -1,8 +1,10 @@
+import { AbstractUserRepository } from '../../user/type/userRepository';
 import { AbstractSubscribeRepository } from './subscribeRepository';
 
 export abstract class AbstractSubscribeService {
 	private static instance: AbstractSubscribeService;
 	private static subscribeRepository: AbstractSubscribeRepository;
+	private static userRepository: AbstractUserRepository;
 
 	public static getInstance(dependency): AbstractSubscribeService;
 	private constructor(dependency);
