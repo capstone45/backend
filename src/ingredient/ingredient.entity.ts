f611fe707076a7a0e8c2945bec2c85d6b2134f1f
wonju-dev <wonjulee.dev@gmail.com>
2022-03-06 18:38:48 +0900

    refacotr : ingredient domain 개선
    미사용 필드 삭제
    레포지토리 생성

--- src/ingredient/ingredient.entity.ts
+++ src/ingredient/ingredient.entity.ts
@@ -1,13 +1,6 @@
 import { Column, Entity, PrimaryColumn, Generated } from 'typeorm';
 
-enum category {
-	CATEGORY1 = 'category1',
-	CATEGORY2 = 'category2',
-	CATEGORY3 = 'category3',
-	CATEGORY4 = 'category4',
-	CATEGORY5 = 'category5',
-	ETC = 'etc',
-}
+import { CreateRecipeDtoIngredient } from '../recipe/type/data';
 
 @Entity({ name: 'INGREDIENT' })
 export default class Ingredient {
@@ -18,6 +11,9 @@ export default class Ingredient {
 	@Column({ name: 'NAME', type: 'varchar', length: 30, nullable: false, unique: true })
 	name: string;
 
-	@Column({ name: 'CATEGORY', type: 'enum', enum: category, nullable: false })
-	category: category;
+	static create(rawIngredient: CreateRecipeDtoIngredient): Ingredient {
+		const ingredient = new Ingredient();
+		ingredient.name = rawIngredient.name;
+		return ingredient;
+	}
 }
