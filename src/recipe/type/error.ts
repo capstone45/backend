c73c09c7cf9750bc29f528cfb95ecfa430347d65
wonju-dev <wonjulee.dev@gmail.com>
2022-03-15 17:33:50 +0900

    refactor : 에러 객체 네이밍 개선

--- src/recipe/type/error.ts
+++ src/recipe/type/error.ts
@@ -1,5 +1,5 @@
 const RecipeError = {
-	NOT_FOUND: { type: 'RECIPE_NOT_FOUND', code: 404 },
+	NOT_FOUND: { message: 'RECIPE_NOT_FOUND', code: 404 },
 };
 
 export default RecipeError;
--- src/user/type/error.ts
+++ src/user/type/error.ts
@@ -1,7 +1,7 @@
 const UserError = {
-	NOT_FOUND: { type: 'USER_NOT_FOUND', code: 404 },
-	NOT_AUTHORIZED: { type: 'NOT_AUTHORIZED', code: 401 },
-	PASSWORD_NOT_MATCH: { type: 'PASSWORD_NOT_MATCH', code: 403 },
+	NOT_FOUND: { message: 'USER_NOT_FOUND', code: 404 },
+	NOT_AUTHORIZED: { message: 'NOT_AUTHORIZED', code: 401 },
+	PASSWORD_NOT_MATCH: { message: 'PASSWORD_NOT_MATCH', code: 403 },
 };
 
 export default UserError;
